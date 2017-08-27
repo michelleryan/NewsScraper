e95b85bb1fb1b77412dd823b879551b93d0b6103
michelleryan <mryan85268@gmail.com>
2017-08-27 16:51:04 -0700

    added some stying on comment

--- public/app.js
+++ public/app.js
@@ -14,18 +14,21 @@
 // displayResults();
 
 /**************************Using mongoose***************** */
-$.getJSON("/headlines", function(data){
-    // for(var i =0; i<data.length; i++){
-    //     $("#headlines").append("<tr><td>" + data[i].title + 
-    //     "</td><td>" + data[i].link + "</td><td>" + data[i].summary + "</td><td>"+ data[i].author + "</td></tr>");
-    // }
-    for(var i=0; i<11; i++){
-                    $("#headlines").prepend("<tr><td>" + data[i].title + 
-            "</td><td>" + data[i].link + "</td><td>" + data[i].summary + "</td><td>"+ data[i].author + 
-            "</td><td><button class='btn waves-effect waves-light' type='button' id='saveComment' value =" + data[i]._id + ">Comment</button>" +"</td></tr>");
-                }
-                $("#headlines").prepend("<tr><th>Title</th><th>Link</th><th>Summary</th><th>Author</th><th>Comment</><tr>");
+
+//when user clicks "Scrape Articles Button, get headlines"
+$(document).on("click", "#scrapeHeadlines", function(){
+    $.getJSON("/headlines", function(data){
+       for(var i=0; i<11; i++){
+            $("#headlines").prepend("<tr><td>" + data[i].title + 
+                "</td><td>" + data[i].link + "</td><td>" + data[i].summary + "</td><td>"+ data[i].author + 
+                "</td><td><button class='btn modal-trigger red darken-2' data-target='#comments' type='button' id='saveComment' value =" + data[i]._id + ">Comment</button>" 
+                +"</td><td><button class='btn waves-effect waves-light red darken-2' type='button' id='save' value =" + data[i]._id + ">Save Article</button>"
+                + "</td></tr>");
+                    }
+                    $("#headlines").prepend("<tr><th>Title</th><th>Link</th><th>Summary</th><th>Author</th></><tr>");
+    });
 });
+
 
 //whenever someone clicks on the comment button
 $(document).on("click", "#saveComment", function(){
@@ -38,16 +41,18 @@ $(document).on("click", "#saveComment", function(){
         url:"/headlines/" + thisId
     }).done(function(data){
         console.log("This is the headline from the ajax GET method", data);
-        //build the html for the comment
-        $("#comments").append("<h2>" + data.title + "<h2>");
-        $("#comments").append("<input id='titleinput name='title'>");
-        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-        $("#comments").append("<button value='" + data._id + "' id='savenote'>Save Comment</button>");
-        
+        // //build the html for the comment
+        // $("#comments").append("<h2>" + data.title + "<h2>");
+        // $("#comments").append("<input id='titleinput name='title'>");
+        // $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
+        // $("#comments").append("<button value='" + data._id + "' id='savenote'>Save Comment</button>");
+        $("#comments").append("<span>" + data.title + "<span>");
+        $("#comments").append("<p><textarea id='bodyinput' class='materialize-textarea' name='body'></textarea></p>");
+        $(".card-action").append("<button value='" + data._id + "' id='savenote'>Save Comment</button>")
 
         // if there is already a comment for the headline, add them to the title and textarea
       if (data.comment) {
-        $("#titleinput").val(data.comment.title);
+        //$("#titleinput").val(data.comment.title);
         $("#bodyinput").val(data.comment.body);
         console.log( $("#bodyinput").val());
       }
@@ -78,10 +83,12 @@ $(document).on("click", "#savenote", function() {
         console.log(data);
         // Empty the notes section
         $("#comments").empty();
+        $(".card-action").empty();
+
       });
   
     // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
+    //$("#titleinput").val("");
     $("#bodyinput").val("");
   });
   
